905e07007051a34f0fe73ea0402fa469fb221d9d
RaimundoJSoares <raianimes1000@gmail.com>
2022-02-18 19:34:29 -0400

    Criando tabela do dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
           py='1'
           color='gray.300'
           borderRightWidth={1}
-          borderColor = 'gray.700'
+          borderColor = 'gray.600'
           >
             <Icon as ={RiNotification2Line} fontSize='20'/>
             <Icon as ={RiUserAddLine} fontSize='20'/>
