5207852385b73f31acccf09f56fd2a4bd97891ee
RaimundoJSoares <raianimes1000@gmail.com>
2022-02-25 20:22:27 -0400

    usando miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import { AppProps } from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SideBarDrawerProvider } from '../contexts/SideBarDrawer'
+import { makeServer } from '../components/services/mirage'
+
+if(process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps } : AppProps) {
   return (
